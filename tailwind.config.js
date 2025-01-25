6fa3182ca11d00f3cb0620883684dac74de5e57a
dimolf345 <dimolfetta.ta@gmail.com>
2025-01-25 11:05:11 +0100

    2 solved tailwind conflicts, start creating landing screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dancing: ["Dancing Script", "serif"],
+      }
+    },
   },
   plugins: [
     require('daisyui')
