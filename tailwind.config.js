ac8974faea47206ecd4b22743542dcbba626d98b
dimolf345 <dimolfetta.ta@gmail.com>
2025-01-25 15:48:08 +0100

    #2 feat: add landing screen to index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     extend: {
       fontFamily: {
         dancing: ["Dancing Script", "serif"],
-      }
+      },
+      keyframes: {
+        fadeOut: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+      },
+      animation: {
+        fadeOut: 'fadeOut 0.5s forwards',
+      },
     },
   },
   plugins: [
